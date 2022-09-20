607c63d3939437e6bf635bc1c6fdc375b6498323
user <user@srv-bb.bb.local>
2022-09-20 19:20:32 +0200

    fix support for youtube links broken

--- init.rb
+++ init.rb
@@ -20,20 +20,27 @@ Redmine::WikiFormatting::Macros.register do
         @num ||= 0
         @num = @num + 1
         attachment = o.attachments.find_by_filename(args[0]) if o.respond_to?('attachments')
-        data_setup = ""
 
         if attachment
             file_url = url_for(:only_path => false, :controller => 'attachments', :action => 'download', :id => attachment, :filename => attachment.filename)
-        else
-            file_url = args[0].gsub(/<.*?>/, '').gsub(/&lt;.*&gt;/,'')
-        end
-out = <<END
+            out = <<END
 <link href="#{request.protocol}#{request.host_with_port}#{ActionController::Base.relative_url_root}/plugin_assets/redmine_embedded_video/video-js.min.css" rel="stylesheet" />
 <script src="#{request.protocol}#{request.host_with_port}#{ActionController::Base.relative_url_root}/plugin_assets/redmine_embedded_video/video.min.js"></script>
 <video controls preload="auto" id="video_#{@num}" class="video-js" width="#{@width}" height="#{@height}">
     <source src="#{file_url}" type="video/mp4" />
 </video>
 END
+        else
+            file_url = args[0].gsub(/<.*?>/, '').gsub(/&lt;.*&gt;/,'')
+            data_setup = '{ "techOrder": ["youtube"], "sources": [{ "type": "video/youtube", "src": "' + file_url + '"}] }'
+            out = <<END
+<link href="#{request.protocol}#{request.host_with_port}#{ActionController::Base.relative_url_root}/plugin_assets/redmine_embedded_video/video-js.min.css" rel="stylesheet" />
+<script src="#{request.protocol}#{request.host_with_port}#{ActionController::Base.relative_url_root}/plugin_assets/redmine_embedded_video/video.min.js"></script>
+<script src="#{request.protocol}#{request.host_with_port}#{ActionController::Base.relative_url_root}/plugin_assets/redmine_embedded_video/Youtube.min.js"></script>
+<video controls preload="auto" id="video_#{@num}" class="video-js" width="#{@width}" height="#{@height}" data-setup='#{data_setup}'>
+</video>
+END
+        end
 
     out.html_safe
   end
